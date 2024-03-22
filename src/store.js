553c333e878abbbae4cfef49e2176f17092de581
Donte Ladatto <donte279@gmail.com>
2024-03-21 22:43:24 -0500

    update actions to consume an API

--- src/store.js
+++ src/store.js
@@ -9,11 +9,38 @@ component that calls it. This will become the only way in Zustand v5. */
 
 const store = (set) => ({
   tasks: [],
+  fetch: async () => {
+    const response = await fetch("http://localhost:3000/todos")
+    set({ tasks: await response.json() })
+  },
   draggedTask: null,
-  addTask: (title, state) => set(store => ({ tasks: [...store.tasks, { title, state }] }), false, 'addTask'),
-  deleteTask: (title) => set(store => ({ tasks: store.tasks.filter(t => t.title !== title) }), false, 'deleteTask'),
-  setDraggedTask: (title) => set({ draggedTask: title }, false, 'setDraggedTask'),
-  moveTask: (title, state) => set(store => ({ tasks: store.tasks.map(t => t.title === title ? { title, state } : t) }), false, 'moveTask')
+  addTask: async (title, state) => {
+    const response = await fetch("http://localhost:3000/todos", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        title: title,
+        state: state
+      })
+    })
+    const newTask = await response.json()
+    set(store => ({ tasks: [...store.tasks, newTask] }), false, 'addTask')
+  },
+  deleteTask: async (id) => {
+    fetch(`http://localhost:3000/todos/${id}`, { method: "DELETE" })
+    set(store => ({ tasks: store.tasks.filter(t => t.id !== id) }), false, 'deleteTask')
+  },
+  setDraggedTask: (id) => set({ draggedTask: id }, false, 'setDraggedTask'),
+  moveTask: async (id, state) => {
+    await fetch(`http://localhost:3000/todos/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        state: state
+      })
+    })
+    set(store => ({ tasks: store.tasks.map(t => t.id === id ? { ...t, state } : t) }), false, 'moveTask')
+  }
 })
 
 export const useStore = createWithEqualityFn(persist(devtools(store), { name: "store" }), shallow)
